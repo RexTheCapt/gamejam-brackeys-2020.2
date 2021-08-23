455f3be2f0853245f03c1f33e59c7a5c62c6ae5e
RexTheCapt <6693554+RexTheCapt@users.noreply.github.com>
2021-08-23 17:49:47 +0200

    Changed ad graphics

---
+++ Assets/Scripts/ChangeImage.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeImage : MonoBehaviour
+{
+    public Sprite sprite = null;
+
+    void Start()
+    {
+        if (sprite == null)
+            throw new System.Exception("Missing sprite");
+
+        gameObject.GetComponent<Image>().sprite = sprite;
+    }
+}
