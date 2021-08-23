b2a98ace61cc11f97b03ef4602073c1a7c35c848
PiotrekUnityDeveloper <85606121+PiotrekUnityDeveloper@users.noreply.github.com>
2021-08-23 13:34:41 +0200

    WINDOWDRAGGING

---
+++ Assets/Scripts/anothertest.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class anothertest : MonoBehaviour, IDragHandler
+{
+    [SerializeField] private RectTransform dragRectTransform;
+    [SerializeField] private Canvas canvas;
+    public void OnDrag(PointerEventData eventData)
+    {
+        dragRectTransform.anchoredPosition += eventData.delta/ canvas.scaleFactor;
+    }
+}
