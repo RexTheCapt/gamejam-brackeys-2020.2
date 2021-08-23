aee33af829ef8914f794e0c9e564defdbd8d672f
RexTheCapt <6693554+RexTheCapt@users.noreply.github.com>
2021-08-23 14:02:06 +0200

    Redisgned ads to work on canvas

--- Assets/Scripts/Ad Stuff/SpawnAds.cs
+++ Assets/Scripts/Ad Stuff/SpawnAds.cs
@@ -2,22 +2,15 @@ using UnityEngine;
 
 public class SpawnAds : MonoBehaviour
 {
-    public Transform CornerTopLeft;
-    public Transform CornerBottomRight;
     public float SpawnTimer = 10;
+    public float _currentTimer;
     public GameObject[] AdList;
-
-    private float _currentTimer;
 
     // Start is called before the first frame update
     void Start()
     {
         if (AdList == null || AdList.Length == 0)
             throw new System.Exception("No ads to spawn");
-
-        Vector2 offset = gameObject.GetComponent<BoxCollider2D>().offset;
-        if (offset.x != 0 || offset.y != 0)
-            Debug.LogWarning("BoxCollider2D offset for ad spawning is not 0,0!");
     }
 
     // Update is called once per frame
@@ -27,7 +20,7 @@ public class SpawnAds : MonoBehaviour
 
         if (_currentTimer > SpawnTimer)
         {
-            Bounds spawnBounds = gameObject.GetComponent<BoxCollider2D>().bounds;
+            var spawnBounds = gameObject.GetComponent<Canvas>().pixelRect;
             Vector2 dimentions = new Vector2(spawnBounds.size.x / 2, spawnBounds.size.y / 2);
 
             // X = Width
@@ -35,7 +28,7 @@ public class SpawnAds : MonoBehaviour
             Vector2 randomDimention = new Vector2(Randomize(dimentions.x, dimentions.x * -1), Randomize(dimentions.y, dimentions.y * -1));
 
             GameObject randomAd = AdList[Random.Range(0, AdList.Length)];
-            Instantiate(randomAd, position: new Vector3(randomDimention.x, randomDimention.y, 0), rotation: transform.rotation);
+            Instantiate(randomAd, position: new Vector3(randomDimention.x, randomDimention.y, 0), rotation: transform.rotation).transform.parent = transform;
 
             _currentTimer -= SpawnTimer;
         }
