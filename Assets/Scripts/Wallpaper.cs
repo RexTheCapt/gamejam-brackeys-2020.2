69a387acf8d1737340fd992920e5c116338e04c7
RexTheCapt <6693554+RexTheCapt@users.noreply.github.com>
2021-08-24 20:06:29 +0200

    Added changeable wallpaper

---
+++ Assets/Scripts/Wallpaper.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Wallpaper : MonoBehaviour
+{
+    public bool ForceUpdateWallPaper = false;
+    public Sprite[] WallPapers;
+    public int WallPaperIndex
+    {
+        get => _wallpaperIndex;
+        set
+        {
+            _wallpaperIndex = value;
+            UpdateWallPaper();
+        }
+    }
+
+    public void Update()
+    {
+        if (ForceUpdateWallPaper)
+        {
+            UpdateWallPaper();
+            ForceUpdateWallPaper = false;
+        }
+    }
+    private void UpdateWallPaper()
+    {
+        gameObject.GetComponent<Image>().sprite = WallPapers[_wallpaperIndex];
+    }
+
+    [SerializeField]
+    private int _wallpaperIndex = 0;
+}
