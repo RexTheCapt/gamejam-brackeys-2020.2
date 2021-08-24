3ad0c55dc14653b232602d9dba154d1e18c39a1e
PiotrekUnityDeveloper <85606121+PiotrekUnityDeveloper@users.noreply.github.com>
2021-08-24 21:01:59 +0200

    MADE AN INTROOOO

---
+++ Assets/Scripts/intromanager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class intromanager : MonoBehaviour
+{
+    public GameObject p4gpanel;
+    public Animator p4g;
+    public GameObject animationcanvas;
+
+    public GameObject secondcanvas;
+    public GameObject skipbutton;
+
+    public GameObject plotPanel;
+    public GameObject missionpanel;
+
+    public Animator missionanimator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(cooldown());
+        p4gpanel.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator cooldown()
+    {
+        yield return new WaitForSecondsRealtime(8f);
+        animationcanvas.SetActive(false);
+        secondcanvas.SetActive(true);
+        yield return new WaitForSecondsRealtime(3.9f);
+        skipbutton.SetActive(true);
+    }
+
+    public void nextclick()
+    {
+        plotPanel.SetActive(false);
+        missionpanel.SetActive(true);
+        StartCoroutine(NextClick());
+    }
+
+    IEnumerator NextClick()
+    {
+        yield return new WaitForSecondsRealtime(0.4f);
+        missionanimator.SetTrigger("out");
+        yield return new WaitForSecondsRealtime(3.7f);
+        SceneManager.LoadScene("Level1");
+    }
+}
