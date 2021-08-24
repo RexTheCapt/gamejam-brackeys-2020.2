fa09b9cfeaca849076ecdd32968eaa5d05d21432
PiotrekUnityDeveloper <85606121+PiotrekUnityDeveloper@users.noreply.github.com>
2021-08-24 14:11:28 +0200

    send test

--- Assets/Scripts/Events.cs
+++ Assets/Scripts/Events.cs
@@ -70,6 +70,45 @@ public class Events : MonoBehaviour
 
     }
 
+    public void SendFinalMessage()
+    {
+        string messagetypedin = messagefield.text.ToString();
+
+        if(StringCompare(messagetypedin, "Dear Customer’s Hell Inc, I wish to complain about your product that I purchased on 29 / 08 / 2021. I am complaining because it is malfunctioning. To resolve this problem I would like you to pay me back the amount of 10 dollars. I look forward to hearing from you as soon as possible. Sincerely, Your Customer") < 90)
+        {
+            Debug.LogWarning("u failed LOLW" + "ur score is: " + StringCompare(messagetypedin, "Dear Customer’s Hell Inc, I wish to complain about your product that I purchased on 29 / 08 / 2021. I am complaining because it is malfunctioning. To resolve this problem I would like you to pay me back the amount of 10 dollars. I look forward to hearing from you as soon as possible. Sincerely, Your Customer"));
+        }
+        else
+        {
+            Debug.Log("u won!");
+            //LOLW
+        }
+
+
+    }
+
+
+    static double StringCompare(string a, string b)
+    {
+        if (a == b) //Same string, no iteration needed.
+            return 100;
+        if ((a.Length == 0) || (b.Length == 0)) //One is empty, second is not
+        {
+            return 0;
+        }
+        double maxLen = a.Length > b.Length ? a.Length : b.Length;
+        int minLen = a.Length < b.Length ? a.Length : b.Length;
+        int sameCharAtIndex = 0;
+        for (int i = 0; i < minLen; i++) //Compare char by char
+        {
+            if (a[i] == b[i])
+            {
+                sameCharAtIndex++;
+            }
+        }
+        return sameCharAtIndex / maxLen * 100;
+    }
+
 
 
 }
