4747347009cc99c9de9014db5322664be980fd33
PiotrekUnityDeveloper <85606121+PiotrekUnityDeveloper@users.noreply.github.com>
2021-08-25 15:51:35 +0200

    added file explorer with some funny files inside

--- Assets/Scripts/WinScreenLoader.cs
+++ Assets/Scripts/WinScreenLoader.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Runtime.InteropServices;
 
 public class WinScreenLoader : MonoBehaviour
 {
@@ -10,12 +11,24 @@ public class WinScreenLoader : MonoBehaviour
 
     public Text exitbuttontext;
 
+    [System.Runtime.InteropServices.DllImport("user32.dll")]
+    private static extern System.IntPtr GetActiveWindow();
+    [DllImport("user32.dll", SetLastError = true)]
+    static extern int MessageBox(int hwnd, string lpText, string lpCaption, uint uType);
+
+    public static System.IntPtr GetWindowHandle()
+    {
+        return GetActiveWindow();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
-        
+        //messagebox test
+        MessageBox(1, "Thanks for playing!", "The end", 0);
     }
+
+
 
     private void Awake()
     {
